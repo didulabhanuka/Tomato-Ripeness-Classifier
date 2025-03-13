a165e5f8315717b7ebb947222dd860817ffefd7d
didulabhanuka <bhanukadidula78@gmail.com>
2025-03-13 13:18:48 +0530

    small updates

--- apps/harvestingpredict/models/yolo_model.py
+++ apps/harvestingpredict/models/yolo_model.py
@@ -1,4 +1,4 @@
 from ultralytics import YOLO
 
 # Load YOLOv8 model
-yolo_model = YOLO("tomato_ripeness_classifier/yolov8_tomato_ripeness/weights/best.pt")
+yolo_model = YOLO("apps/harvestingpredict/yolov8_tomato_ripeness/weights/best.pt")
